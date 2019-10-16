b3bf0fb29481a1011276e0ead399a208d2aea4fe
Tsukiyonocm <tsukiyonocm@yahoo.com>
2019-10-15 23:03:51 -0400

    First Commit

---
+++ edabit.py
@@ -0,0 +1,62 @@
+# Practice Problems pulled from Edabit.com
+
+# Difficulty Very Easy
+# ========================================================
+
+# Question 1 - Return the Sum of Two Numbers
+# Create a function that takes two numbers as arguments
+# and Return their sum.
+
+# Example:
+# addition(3, 2) ==> 5
+# addition(-3, -6) ==> -9
+# addition(7, 3) ==> 10
+
+def addition(x, y):
+    return x + y
+
+# Testers
+# addition(3, 2)
+# addition(-3, -6)
+# addition(7, 3)
+
+# ---------------------------------------------------------
+# ---------------------------------------------------------
+
+
+# Question 2 - Minutes to Seconds
+# Create a function that takes an integer [minutes] and
+# converts it to seconds.
+
+# Example:
+# convert(5) ==> 300 
+# convert(3) ==> 180
+# convert(2) ==> 120
+
+def convert(num):
+    return num * 60
+
+# Testers
+# convert(5)
+# convert(3)
+# convert(2)
+
+# ---------------------------------------------------------
+# ---------------------------------------------------------
+
+# Question 3 - Return the Next Number from the Integer Passed
+# Create a function that takes a number as an argument, increments
+# the number by +1 and returns the result.
+
+# Example:
+# addition(0) ==> 1 
+# addition(9) ==> 10
+# addition(-3) ==> -2
+
+def addition(num):
+    return num + 1
+
+# Testers
+addition(0) 
+addition(9)
+addition(-3)
---
+++ q_directory.py
@@ -0,0 +1,4 @@
+# Edabit Questions
+#   - Very Easy
+        # 1. Return the Sum of Two Numbers
+        # 2. Minutes to Seconds
